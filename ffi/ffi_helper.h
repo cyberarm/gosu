c819695bc1450459569197fdc7897ff30aa9b103
Cyberarm <matthewlikesrobots@gmail.com>
2021-01-02 13:04:53 -0600

    Updated ffi_helper.h to support more than just msvc

--- ffi/ffi_helper.h
+++ ffi/ffi_helper.h
@@ -1,7 +1,24 @@
 #pragma once
 
-#ifdef GOSUFFI_EXPORTS
-#define GOSU_FFI_API __declspec(dllexport)
+// https://stackoverflow.com/a/2164853
+
+/* Microsoft */
+#if defined(_MSC_VER)
+#define GOSU_FFI_API_EXPORTS __declspec(dllexport)
+#define GOSU_FFI_API_IMPORTS __declspec(dllimport)
+/* GCC */
+#elif defined(__GNUC__)
+#define GOSU_FFI_API_EXPORTS __attribute__((visibility("default")))
+#define GOSU_FFI_API_IMPORTS
+/* do nothing and hope for the best? */
 #else
-#define GOSU_FFI_API __declspec(dllimport)
+#define GOSU_FFI_API_EXPORTS
+#define GOSU_FFI_API_IMPORTS
+#pragma warning Unknown dynamic link import/export semantics.
+#endif
+
+#if GOSUFFI_EXPORTS
+#define GOSU_FFI_API GOSU_FFI_API_EXPORTS
+#else
+#define GOSU_FFI_API GOSU_FFI_API_IMPORTS
 #endif
